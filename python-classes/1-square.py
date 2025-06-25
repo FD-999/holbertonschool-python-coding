ff56114817840bae6f6260270982104c8387b890
Your Name <you@example.com>
2025-06-25 14:45:50 +0000

    54

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-"""fuad"""
+"""Defines a Square class."""
 
 
 class Square:
-    """baba"""
+    """Represents a square."""
 
     def __init__(self, size=0):
-        """babaa"""
-        if type(size) != int:
-
+        """Initializes the square with validation."""
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        is size < 0:
-
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
