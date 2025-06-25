0105c5b1066300634a6396d99139976745755f77
Your Name <you@example.com>
2025-06-25 14:42:26 +0000

    54

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -11,7 +11,14 @@ class Square:
 
 
         if type(size) != int:
+            """fd"""
+
+
             raise TypeError("size must be an integer")
         is size < 0:
+            """fd"""
+
+
+
             raise ValueError("size must be >= 0")
         self.__size = size
